decf28d5a86170730bd857ed70ff3a4ef208e407
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-03-17 17:31:39 +0300

    WIP input delay

--- src/test/java/WebDriverSeleniumTest.java
+++ src/test/java/WebDriverSeleniumTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,24 +24,34 @@ public class WebDriverSeleniumTest {
     }
 
     @Test (description = "selenium search")
-    public void searchResultNotEmpty() {
+    public void searchResultSelenium() throws InterruptedException {
         driver.get("https://selenium.dev");
 
         WebElement searchInput = driver.findElement(By.xpath("//input[@type='search']"));
-        searchInput.sendKeys("test" + Keys.ENTER);
+        searchInput.sendKeys("test");
+
+        Thread.sleep(5000);//debug
+
+        searchInput.sendKeys(Keys.ENTER);
+
         WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(30))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//div/h1")));
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//div[1]/h1")));
         System.out.println(firstResult.getText());
+
+        Assert.assertTrue(firstResult.isDisplayed(), "No search results.");
+
     }
 
     @Test (description = "chrome search")
     public void searchResultChrome() {
         driver.get("https://google.com");
 
-        driver.findElement(By.name("q")).sendKeys("selenium" + Keys.ENTER);
+        driver.findElement(By.name("q")).sendKeys("A" + Keys.ENTER);
         WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(15))
                 .until(ExpectedConditions.elementToBeClickable(By.id("result-stats")));
         System.out.println(firstResult.getText());
+
+        Assert.assertTrue(firstResult.isDisplayed(), "No search results.");
     }
 
 
